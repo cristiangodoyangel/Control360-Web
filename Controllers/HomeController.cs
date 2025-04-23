7e1325d4e7247bd18d96495ea2c35097405abcc4
cristiangodoyangel <creacciones@gmail.com>
2025-04-23 12:32:45 -0400

    se crea inventariodbcontext, se agrega codigo a homecontroller para hacer check a la conexión de db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.Mvc;
 
 namespace Inventario360.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewData["Message"] = "Your application description page.";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult Index()
+        {
+            bool conexionExitosa = false;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    conn.Open();
+                    conexionExitosa = (conn.State == ConnectionState.Open);
+                }
+            }
+            catch (Exception ex)
+            {
+                conexionExitosa = false;
+                ViewBag.Error = ex.Message; // opcional para debug
+            }
+
+            ViewBag.Conectado = conexionExitosa;
             return View();
         }
     }
---
+++ Data/InventarioDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Entity;
+using Inventario360.Web.Models;
+
+namespace Inventario360.Web.Data
+{
+    public class InventarioDbContext : DbContext
+    {
+        public InventarioDbContext() : base("DefaultConnection") { }
+
+        /*public DbSet<SalidaDeBodega> SalidaDeBodega { get; set; }
+        public DbSet<DetalleSalidaDeBodega> DetalleSalidaDeBodega { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<SolicitudDeMaterial> SolicitudDeMaterial { get; set; }
+        public DbSet<Empleado> Empleado { get; set; }
+        public DbSet<Proveedor> Proveedor { get; set; }
+        public DbSet<Proyecto> Proyecto { get; set; }
+        public DbSet<FichaEmpleado> FichaEmpleado { get; set; }
+        public DbSet<Camioneta> Camionetas { get; set; }
+        public DbSet<FichaCamioneta> FichaCamionetas { get; set; }*/
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Relaciones personalizadas si las necesitas
+           /* modelBuilder.Entity<SalidaDeBodega>()
+                .HasRequired(s => s.SolicitanteObj)
+                .WithMany()
+                .HasForeignKey(s => s.Solicitante);
+
+            modelBuilder.Entity<SalidaDeBodega>()
+                .HasRequired(s => s.ResponsableEntregaObj)
+                .WithMany()
+                .HasForeignKey(s => s.ResponsableEntrega);
+
+            modelBuilder.Entity<SalidaDeBodega>()
+                .HasRequired(s => s.ProyectoObj)
+                .WithMany()
+                .HasForeignKey(s => s.ProyectoAsignado);*/
+        }
+    }
+}
